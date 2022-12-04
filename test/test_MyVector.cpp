aefef2c4b0f9107c98fd6afa721d6c9a74d91d87
OsipovDN <disorder_2@list.ru>
2022-12-04 17:05:31 +0300

    Подготовленный шаблон для работы

--- test/test_MyVector.cpp
+++ test/test_MyVector.cpp
@@ -1,6 +1,30 @@
 #include <gtest/gtest.h>
 #include "myVector.h"
 
+TEST(Vector, Size) {
+    MyVector <int> Vec;
+
+    ASSERT_EQ(Vec.size(), 0);
+
+}
+
+TEST(Vector, PushBack) {
+    // Arrange
+    const size_t count = 10;
+    size_t val= 20;
+    MyVector <size_t> Vec;
+
+    // Act
+    for (size_t i = 0; i < count; ++i) {
+        Vec.push_back(i);
+    }
+    Vec.push_back(val);
+
+    // Assert
+    ASSERT_EQ(Vec.size(), count+1);
+    ASSERT_EQ(Vec[10], val);
+}
+
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
