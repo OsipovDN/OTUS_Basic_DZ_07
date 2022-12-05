6884e703bd844a7189cd4f4ed65ffffdba2e1b7b
OsipovDN <disorder_2@list.ru>
2022-12-05 23:35:32 +0300

    Доработка тестов для вектора

--- test/test_ImplVector.cpp
+++ test/test_ImplVector.cpp
@@ -2,7 +2,7 @@
 #include "ImplVector.h"
 #include <iostream>
 
-TEST(Vector, Size) {
+TEST(Vector, InitializationContainer) {
     // Arrange
     ImplVector <int> Vec;
     // Assert
@@ -63,7 +63,6 @@ TEST(Vector, InsertInMiddle) {
 TEST(Vector, EraseEnd) {
     // Arrange
     const size_t count = 10;
-    size_t size = 9;
     ImplVector <size_t> Vec;
     ImplVector <size_t> VecRes;
     for (size_t i = 0; i < count; ++i) {
@@ -80,6 +79,80 @@ TEST(Vector, EraseEnd) {
     ASSERT_TRUE(Vec==VecRes);
 }
 
+TEST(Vector, EraseFront) {
+    // Arrange
+    const size_t count = 10;
+    ImplVector <size_t> Vec;
+    ImplVector <size_t> VecRes;
+    for (size_t i = 0; i < count; ++i) {
+        Vec.push_back(i);
+        if (i != 0)
+            VecRes.push_back(i);
+    }
+
+    // Act
+    Vec.erase(1);
+
+    // Assert
+    std::cout << "Checking the size after deleting the first element" << std::endl;
+    ASSERT_TRUE(Vec == VecRes);
+}
+
+TEST(Vector, EraseMidlle) {
+    // Arrange
+    const size_t count = 11;
+    ImplVector <size_t> Vec;
+    ImplVector <size_t> VecRes;
+    for (size_t i = 0; i < count; ++i) {
+        Vec.push_back(i);
+        if (i != 5)
+            VecRes.push_back(i);
+    }
+
+    // Act
+    Vec.erase(6);
+
+    // Assert
+    std::cout << "Checking the removal of an element from the middle" << std::endl;
+    ASSERT_TRUE(Vec == VecRes);
+}
+
+TEST(Vector, OperatorGetElement) {
+    // Arrange
+    const size_t count = 10;
+    size_t val = 0;
+    ImplVector <size_t> Vec;
+    for (size_t i = 0; i < count; ++i) {
+        Vec.push_back(i);
+    }
+
+    // Act
+    for (size_t i = 0; i < count; ++i) {
+        if (Vec[i] != i) 
+            break;
+        val++;
+    }
+
+    // Assert
+    std::cout << "Checking the size of the container" << std::endl;
+    ASSERT_EQ(Vec.size(),val);
+}
+
+TEST(Vector, GetSize) {
+    // Arrange
+    const size_t count = 10;
+    const size_t size = 10;
+    ImplVector <size_t> Vec;
+    for (size_t i = 0; i < count; ++i) {
+        Vec.push_back(i);
+    }
+
+    // Act
+
+    // Assert
+    std::cout << "Checking the size of the container" << std::endl;
+    ASSERT_EQ(Vec.size(), size);
+}
 
 
 int main(int argc, char** argv) {
