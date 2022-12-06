11625d77501e2a8bfbbbad88e1c3123d0ad59750
OsipovDN <disorder_2@list.ru>
2022-12-06 16:15:15 +0300

    Тесты для контейнера List

---
+++ test/test_ImplList.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "ImplList.h"
+#include <iostream>
+
+TEST(List, InitializationContainer) {
+
+}
+
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
